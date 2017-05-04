b31a1d034f0e4065693d64b1e291f3f987a38a4b
Daniel <danaguilar6@gmail.com>
2017-05-04 14:07:08 -0500

    Added presence, length, and format validation to the name and email fields of the User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,41 @@ class UserTest < ActiveSupport::TestCase
     @user.name = ''
     assert_not  @user.valid?
   end
+
+  test "email should be presense" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 255 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid email addresses" do
+    valid_address = %w[example@test.com EiTHE_3e@another.com first.last@name.com weird-dashes-andThings@gmail.com]
+    valid_address.each  do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid email addresses" do
+    invalid_address = %w[example@test,com another.com This-one-sucks@gmail for@bar..com]
+    invalid_address.each do |address|
+      @user.email =  address
+      assert_not @user.valid?, "#{address} should not be valid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
